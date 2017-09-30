2bdd9c787519fc5dc502558c5aca2d42f21a6257
aleksacastle <aaklochko@gmail.com>
2017-09-30 21:26:09 +0300

    postgresql and rspec config

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,16 @@ module Tickets
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    # generate system test files.
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
