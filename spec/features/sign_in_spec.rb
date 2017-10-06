fa430205fe79e08b6cfdc7ef01d3fb6a4d0f4731
aleksacastle <aaklochko@gmail.com>
2017-10-06 20:02:19 +0300

    add sign in test feature

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 describe "the signin process", :type => :feature do
   before :each do
-    User.make(email: 'user@example.com', password: 'password')
+    User.create(email: 'user@example.com', password: 'password')
   end
 
   it "signs me in" do
-    visit '/sessions/new'
-    within("#session") do
-      fill_in 'Email', with: 'user@example.com'
-      fill_in 'Password', with: 'password'
-    end
-    click_button 'Sign in'
-    expect(page).to have_content 'Success'
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
   end
 end
