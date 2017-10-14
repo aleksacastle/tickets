435e5fc016d925250427eed830f4fb3488ab0b71
aleksacastle <aaklochko@gmail.com>
2017-10-14 16:22:22 +0300

    extract render buses to private method

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,6 +1,12 @@
 class BusesController < ApplicationController
   def index
     buses = Bus.all
-    render 'buses/index', :locals => { buses: buses}
+    render_buses(buses)
   end
+
+  private
+
+    def render_buses(buses)
+      render 'buses/index', :locals => { buses: buses}
+    end
 end
