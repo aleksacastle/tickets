6077bfd63974fde958795d6987d5c261d43c43f5
aleksacastle <aaklochko@gmail.com>
2017-10-15 13:27:58 +0300

    add bus new action, view, partial form

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,4 +1,9 @@
 class BusesController < ApplicationController
+  def new
+    bus = Bus.new
+    render_bus_form(bus)
+  end
+
   def index
     buses = Bus.all
     render_buses(buses)
@@ -8,5 +13,9 @@ class BusesController < ApplicationController
 
     def render_buses(buses)
       render 'buses/index', :locals => { buses: buses}
+    end
+
+    def render_bus_form(bus)
+      render 'buses/new', :locals => { bus: bus}
     end
 end
